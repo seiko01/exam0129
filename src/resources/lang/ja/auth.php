eec23c3ea9d9c8486ea1fd14693cba80957410ac
seiko <seiko.iwao.0715@gmail.com>
2025-01-30 17:23:05 +0900

    Controllerの修正

--- src/resources/lang/ja/auth.php
+++ src/resources/lang/ja/auth.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'failed'   => '認証情報と一致するレコードがありません。',
+
     'required' => ':attribute は必須です。',
         'email' => ':attribute は「ユーザー名@ドメイン」形式で入力してください。',
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'index']);
 });
